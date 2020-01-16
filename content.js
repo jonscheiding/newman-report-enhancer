d8dee01eec3c988e13737b7e1774503023ecfdec
Jonathan Scheiding <jscheiding@certicasolutions.com>
2020-01-16 09:57:36 -0500

    Refactor for readability

--- content.js
+++ content.js
@@ -1,42 +1,83 @@
-function highlightFailures() {
+function highlightFailedAssertions(panel) {
+  const assertionRows = $(panel).find('table > tbody > tr');
+  assertionRows.each((i, row) => {
+    const assertionFailedCount = parseInt(
+      $(row)
+        .find('td:nth-child(3)')
+        .text()
+    );
+
+    if(assertionFailedCount > 0) {
+      $(row).addClass('failed');
+    }
+  })
+}
+
+function highlightFailedTests() {
   $('.panel').each((i, panel) => {
     const failedCount = parseInt(
       $(panel)
         .find(':contains("Total failed tests")')
-        .next()
-        .text()
+        .next().text()
     );
 
-    const collapseTarget = $(panel).find('.panel-collapse');
-    
     if(failedCount == 0) {
-      // $(panel).find('a[data-toggle="collapse"]').trigger('click');
-      // window.postMessage({
-      //   type: 'collapse',
-      //   action: 'hide',
-      //   id: $(panel).attr('id')
-      // }, '*');
-      $(collapseTarget).collapse('hide');
       return;
     }
 
-    $(collapseTarget).collapse('show');
+    $(panel).addClass('failed');
 
-    $(panel).find('.panel-heading').addClass('failed');
-
-    const assertionRows = $(panel).find('table > tbody > tr');
-    assertionRows.each((i, row) => {
-      const assertionFailedCount = parseInt(
-        $(row)
-          .find('td:nth-child(3)')
-          .text()
-      );
-
-      if(assertionFailedCount > 0) {
-        $(row).addClass('failed');
-      }
-    })
+    highlightFailedAssertions(panel);
   });
+}
+
+function togglePanelCollapseBasedOnTestResult() {
+  $('.panel.failed .panel-collapse').collapse('show');
+  $('.panel:not(.failed) .panel-collapse').collapse('hide');
+}
+
+function highlightFailures() {
+  highlightFailedTests();
+  togglePanelCollapseBasedOnTestResult();
+}
+
+function createHighlightFailuresButton(parent) {
+  parent.append(
+    $('<button />')
+      .addClass('btn btn-danger')
+      .attr('data-action', 'highlight-failures')
+      .text('Highlight Failures')
+      .click(highlightFailures)
+  );
+}
+
+function createCollapsePlugin() {
+  embedScriptInHostPage(chrome.runtime.getURL('embed.js'));
+
+  //
+  // Create a jQuery plugin function that uses postMessage to talk to 
+  // the host page for collapsing/expanding panels.
+  //
+  // See the other side of this communication in embed.js
+  //
+  $.fn.collapse = function(action) {
+    this.each((i, element) => {
+      window.postMessage({
+        type: 'collapse',
+        action: action,
+        id: element.id
+      }, '*');
+    });
+    
+    return this;
+  }  
+}
+
+function embedScriptInHostPage(url) {
+  var s = document.createElement('script');
+  s.src = url;
+  s.type = 'text/javascript';
+  (document.head || document.documentElement).appendChild(s);
 }
 
 $(() => {
@@ -45,28 +86,7 @@ $(() => {
     return;
   }
 
-  checkElement.append(
-    $('<button />')
-      .addClass('btn btn-danger')
-      .attr('data-action', 'highlight-failures')
-      .text('Highlight Failures')
-      .click(highlightFailures)
-  );
+  createHighlightFailuresButton(checkElement);
 
-  var s = document.createElement('script');
-  s.src = chrome.runtime.getURL('embed.js');
-  s.type = 'text/javascript';
-  (document.head || document.documentElement).appendChild(s);
+  createCollapsePlugin();
 });
-
-$.fn.collapse = function(action) {
-  this.each((i, element) => {
-    window.postMessage({
-      type: 'collapse',
-      action: action,
-      id: element.id
-    }, '*');
-  });
-  
-  return this;
-}
